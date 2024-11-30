2378f7d9a08a80e1d9395ef8852a864a47aff19c
elena.ryabova77m@gmail.com <elena.ryabova77m@gmail.com>
2024-11-30 12:52:41 +0300

    настройка структуры

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"server/internal/hello"
+)
+
+func main() {
+	router := http.NewServeMux()
+	hello.NewHelloHandler(router)
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
+
+}
